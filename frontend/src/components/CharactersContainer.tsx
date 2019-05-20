09fa1f3f12f7cc73544644e5718823b05058f6da
Byron Guina <baguina@redstoneconsultinggroup.org>
2019-05-20 14:14:55 -0500

    Use the HTTP endpoint in development to avoid cert issues

--- frontend/src/components/CharactersContainer.tsx
+++ frontend/src/components/CharactersContainer.tsx
@@ -1,43 +1,47 @@
-import React from 'react';
-import { RouteComponentProps } from 'react-router';
-import { Character, Party } from './Types';
-import styled from 'styled-components';
-import CharacterCard from './CharacterCard';
+import React from "react";
+import { RouteComponentProps } from "react-router";
+import { Character, Party } from "./Types";
+import styled from "styled-components";
+import CharacterCard from "./CharacterCard";
 
 type Params = {
-    id: string;
+  id: string;
 };
 
 const CharactersContainer = (props: RouteComponentProps<Params>) => {
-    const [characters, setCharacters] = React.useState<Character[]>([]);
-    const { id: partyId } = props.match.params;
+  const [characters, setCharacters] = React.useState<Character[]>([]);
+  const { id: partyId } = props.match.params;
 
-    // Technically we have this data already in `PartiesContainer`, but we want
-    // to fetch it here instead of passing down props in the case that a user
-    // comes to this route directly. If they come directly and we _don't_ fetch,
-    // then the data won't exist in the location.state prop and nothing will render.
-    React.useEffect(() => {
-        const getPartyCharacters = async () => {
-            // Normally there would be more robust error handling here
-            const response = await fetch(`https://localhost:5001/api/party/${partyId}`);
-            const result: Party = await response.json();
-            setCharacters(result.characters);
-        };
+  // Technically we have this data already in `PartiesContainer`, but we want
+  // to fetch it here instead of passing down props in the case that a user
+  // comes to this route directly. If they come directly and we _don't_ fetch,
+  // then the data won't exist in the location.state prop and nothing will render.
+  React.useEffect(() => {
+    const getPartyCharacters = async () => {
+      // Normally there would be more robust error handling here
+      const response = await fetch(
+        `http://localhost:5000/api/party/${partyId}`,
+      );
+      const result: Party = await response.json();
+      setCharacters(result.characters);
+    };
 
-        getPartyCharacters();
-    }, [partyId]);
+    getPartyCharacters();
+  }, [partyId]);
 
-    const Characters = characters.map((character) => <CharacterCard character={character} />);
+  const Characters = characters.map((character) => (
+    <CharacterCard character={character} />
+  ));
 
-    return <Container>{Characters}</Container>;
+  return <Container>{Characters}</Container>;
 };
 
 const Container = styled.main`
-    display: flex;
-    justify-content: center;
-    flex-wrap: wrap;
-    max-width: 1400px;
-    margin: 0 auto;
+  display: flex;
+  justify-content: center;
+  flex-wrap: wrap;
+  max-width: 1400px;
+  margin: 0 auto;
 `;
 
 export default CharactersContainer;
--- frontend/src/components/PartiesContainer.tsx
+++ frontend/src/components/PartiesContainer.tsx
@@ -1,45 +1,45 @@
-import React from 'react';
-import styled from 'styled-components';
-import PartyCard from './PartyCard';
-import { Party } from './Types';
+import React from "react";
+import styled from "styled-components";
+import PartyCard from "./PartyCard";
+import { Party } from "./Types";
 
 const PartiesContainer = () => {
-    const [parties, setParties] = React.useState<Party[]>([]);
+  const [parties, setParties] = React.useState<Party[]>([]);
 
-    React.useEffect(() => {
-        const loadParties = async () => {
-            // Normally there would be more robust error handling here
-            const response = await fetch('https://localhost:5001/api/party');
-            const result: Party[] = await response.json();
-            setParties(result);
-        };
+  React.useEffect(() => {
+    const loadParties = async () => {
+      // Normally there would be more robust error handling here
+      const response = await fetch("http://localhost:5000/api/party");
+      const result: Party[] = await response.json();
+      setParties(result);
+    };
 
-        loadParties();
-    }, []);
+    loadParties();
+  }, []);
 
-    const partyCards = parties.map((party) => <PartyCard party={party} />);
+  const partyCards = parties.map((party) => <PartyCard party={party} />);
 
-    return (
-        <Container>
-            <CardsContainer>{partyCards}</CardsContainer>
-        </Container>
-    );
+  return (
+    <Container>
+      <CardsContainer>{partyCards}</CardsContainer>
+    </Container>
+  );
 };
 
 const Container = styled.main`
-    display: flex;
-    flex-direction: column;
-    max-width: 900px;
-    margin: 0 auto;
+  display: flex;
+  flex-direction: column;
+  max-width: 900px;
+  margin: 0 auto;
 `;
 
 const CardsContainer = styled.div`
-    display: flex;
-    align-content: center;
-    justify-content: center;
-    flex-wrap: wrap;
+  display: flex;
+  align-content: center;
+  justify-content: center;
+  flex-wrap: wrap;
 
-    max-width: 1400px;
+  max-width: 1400px;
 `;
 
 export default PartiesContainer;
