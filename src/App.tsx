1f059682c899d3b9aa57e1bda82d9a27b5112dec
ByronGuina <byron.guina@gmail.com>
2019-05-21 23:51:32 -0500

    CreateGlobalStyles causes issues with TypeScript
    We lose some critical CSS injection :(

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
-import styled, { createGlobalStyle } from 'styled-components';
 
 import Navigation from './components/Navigation';
 import routes from './routes';
+
+import './App.css';
 
 const App = () => {
     const Routes = routes.map((route) => (
@@ -12,42 +13,10 @@ const App = () => {
 
     return (
         <>
-            {/* Not sure why there's a typescript error here... */}
-            <GlobalStyles />
             <Navigation />
             <Switch>{Routes}</Switch>
         </>
     );
 };
-
-// Create some simple baseline styles
-const GlobalStyles = createGlobalStyle`
-    @import url('https://rsms.me/inter/inter.css');
-
-    body {
-        background-color: hsla(0, 0%, 98%, 1);
-        font-variant-numeric: tabular-nums;
-
-    }
-
-    html {
-        font-size: 1.125;
-        line-height: 1.5;
-        font-family: 'Inter', sans-serif;
-        font-variant-numeric: tabular-nums;
-        font-feature-settings: 'zero' 1;
-        color: hsla(0, 0%, 21%, 1);
-    }
-
-    @supports (font-variation-settings: normal) {
-        html { font-family: 'Inter var', sans-serif; }
-    }
-
-    a {
-        text-decoration: none;
-        font-size: 1.6rem;
-        color: hsla(0, 0%, 21%, 1);
-    }
-`;
 
 export default App;
